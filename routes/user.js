24293f98eb47fc82f3fe7dce14c0556cd9b9026b
Allan James Lapid <ajLapid718@gmail.com>
2018-04-25 14:32:25 -0400

    Before TESTING POST

--- routes/user.js
+++ routes/user.js
@@ -2,3 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = router;
+
+// router.get("/users", function(req, res, next) {
+//
+// })
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
+const models = require('../models');
+const Page = models.Page;
+const User = models.User;
+
 module.exports = router;
 
+function changer(urlTitle) {
+  if (urlTitle) return urlTitle.replace(/\s+/g, "_").replace(/[\W\d]/g, "");
+  else return Math.random().toString(36).substring(2,7);
+}
+
+Page.beforeValidate((pageInstance, optionsObject) => {
+  pageInstance.urlTitle = changer(pageInstance.title);
+})
+
 router.get("/", function(req, res, next) {
-  res.send("Hello world1");
+  res.render("wikipage");
 });
 
-router.post("/", function(req, res, next) {
-  res.send("Hello world2");
+router.get("/", function(req, res, next) {
+  res.redirect("/");
 });
 
 router.get("/add", function(req, res, next) {
-  res.send("Hello world3");
+  res.render("addpage");
+});
+
+router.post("/", function(req, res, next) {
+  const page = Page.build({
+    title: "Lunch",
+    content: "Having lunch!"
+  })
+
+  page.save();
 });
